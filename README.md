6f7ccbfbc6136b0075ad78344cbe7fafa48f417e
CSLukeW <williams.luke.2001@gmail.com>
2020-06-10 17:46:30 -0600

    Switched to tensorflow from pytorch

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Built in python 3.6.9
 
 Will use data from Alpha Vantage API to inform model.
+Model powered by tensorflow.
--- trainer.py
+++ trainer.py
@@ -1,46 +1,46 @@
 """ training script """
 
 import pandas as pd
-import numpy
+import numpy as np
 import argparse
-import torch
+import tensorflow as tf
 
 import data_helpers as dh
 
-parser = argparse.ArgumentParser(description="Model Training Script")
-parser.add_argument('key', help='User API Key')
-parser.add_argument('-outdir', metavar='out', default='/models/', help="Directory for stored model(s) (one for each symbol).")
-parser.add_argument('symbols', nargs=argparse.REMAINDER, help="List of symbols to train (Place all at end of command)")
-parse = parser.parse_args()
+if __name__ == '__main__':
 
-data = {}
+    parser = argparse.ArgumentParser(description="Model Training Script")
+    parser.add_argument('key', help='User API Key')
+    parser.add_argument('-outdir', metavar='out', default='/models/', help="Directory for stored model(s) (one for each symbol).")
+    parser.add_argument('symbols', nargs=argparse.REMAINDER, help="List of symbols to train (Place all at end of command)")
+    parse = parser.parse_args()
 
-for symbol in parse.symbols:
+    data = {}
 
-    # read historical daily data from alpha_vantage
-    # store in python dict
-    hist = dh.daily(symbol, parse.key, compact=False)
-    data[symbol] = hist
-    #print(hist)
-    #print()
+    for symbol in parse.symbols:
 
-    """ Data Preprocessing """ 
+        # read historical daily data from alpha_vantage
+        # store in python dict
+        hist = dh.daily(symbol, parse.key, compact=False)
+        data[symbol] = hist
+        #print(hist)
+        #print()
 
-    # turn dataframe to numpy array
-    tmp = hist.to_numpy()
+        """ Data Preprocessing """ 
 
-    # split into training and testing sets 90-10
-    split = round(tmp.shape[0]*1/10)
-    test, training = tmp[:split], tmp[split:]
-    #print(training.shape)
-    #print(test.shape)
+        # turn dataframe to numpy array
+        tmp = hist.to_numpy()
 
-    # convert numpy arrays to PyTorch tensors
-    training_tensor = torch.from_numpy(training)
-    test_tensor = torch.from_numpy(test)
-    
-    """ -------------------------------- """
+        # split into training and testing sets 90-10
+        split = round(tmp.shape[0]*1/10)
+        test, training = tmp[:split], tmp[split:]
+        #print(training.shape)
+        #print(test.shape)
 
-    torch.set_printoptions(sci_mode=False, precision=2)
-    print(training_tensor)
-    print(test_tensor)
+        # convert numpy arrays to PyTorch tensors
+        training_tensor = tf.convert_to_tensor(training, np.float32)
+        test_tensor = tf.convert_to_tensor(test, np.float32)
+        
+        """ -------------------------------- """
+        print(training_tensor)
+        print(test_tensor)
