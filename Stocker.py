7d30d86a4579b988ca23d0f7e92cafd4881c9fb0
CSLukeW <williams.luke.2001@gmail.com>
2020-06-10 23:27:20 -0600

    Added WIP training function for Stocker class.

--- Stocker.py
+++ Stocker.py
@@ -9,17 +9,35 @@ from matplotlib import pyplot
 import data_helpers as dh
 
 class Stocker:
-    def __init__(self, shape, loss='mse', optimizer='adam'):
+    def __init__(self, training, test, loss='mse', optimizer='adam'):
         """ Creating Stocker instance immediately creates model 
 
             Model (WIP) is a two-layer LSTM. Defaults to Mean Squared Error
             loss function and ADAM optimizer function.
         """
+        self.training_data = training
+        self.test_data = test
+
         self.model = tf.keras.Sequential()
-        self.model.add(tf.keras.layers.LSTM(100, input_shape=(shape)))
+        self.model.add(tf.keras.layers.LSTM(100, activation='tanh', recurrent_activation='sigmoid', \
+                                            input_shape=(training.shape[1], training.shape[2])))
         self.model.add(tf.keras.layers.Dense(5))
         self.model.compile(loss=loss, optimizer=optimizer)
         print(self.model.summary())
+
+    """ Training WIP
+
+    def train(self):
+        self.fit = self.model.fit(self.training_data, epochs=50, \
+                            batch_size=100, \
+                            validation_data=self.test_data, verbose=2, shuffle=False)
+
+        pyplot.plot(self.fit['loss'], label='train')
+        pyplot.plot(self.fit['val_loss'], label='test')
+        pyplot.legend()
+        pyplot.show()
+        
+    """
 
 
 if __name__ == '__main__':
@@ -40,7 +58,7 @@ if __name__ == '__main__':
         # store in python dict
         hist = dh.daily(symbol, parse.key, compact=False)
         data[symbol] = hist
-        #print(hist)
+        print(hist)
         #print()
 
         """ Data Preprocessing """ 
@@ -51,17 +69,20 @@ if __name__ == '__main__':
         # split into training and testing sets 90-10
         split = round(tmp.shape[0]*1/10)
         test, training = tmp[:split], tmp[split:]
-        #print(training.shape)
-        #print(test.shape)
 
-        # convert numpy arrays to PyTorch tensors
+        test = dh.array_to_supervised(test, 3)
+        test = test.reshape((test.shape[0], 1, test.shape[1]))
+
+        training = dh.array_to_supervised(training, 3)
+        training = training.reshape((training.shape[0], 1, training.shape[1]))
+
+        # convert numpy arrays to tensors and reshape for LSTM
         training_tensor = tf.convert_to_tensor(training, np.float32)
         test_tensor = tf.convert_to_tensor(test, np.float32)
-
-        #print(training_tensor.shape)
         
         """ -------------------------------- """
         #print(training_tensor)
         #print(test_tensor)
 
-        model = Stocker(training_tensor.shape)
+        model = Stocker(training_tensor, test_tensor)
+        model.train()
