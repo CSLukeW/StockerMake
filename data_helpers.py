d08d402d0b5797fb2eebdcf6edc3794f9e23b141
CSLukeW <williams.luke.2001@gmail.com>
2020-06-10 23:12:32 -0600

    Function to convert array to supervised.

--- data_helpers.py
+++ data_helpers.py
@@ -1,4 +1,6 @@
 from alpha_vantage.timeseries import TimeSeries
+import numpy as np
+from scipy.ndimage.interpolation import shift
 
 def daily(symbol, key, compact=True):
     """ Returns data frame of queried data
@@ -16,3 +18,32 @@ def daily(symbol, key, compact=True):
         data, _ = ts.get_daily(symbol=symbol, outputsize='full')
 
     return data
+
+def array_to_supervised(array, input_index):
+    """ 
+        Converts given numpy array to supervised learning input.
+
+        Shifts given to t-1 and moves it to end of the array. 
+        End column represents the recursive input value.
+    """
+
+    temp = []
+
+    for row in array:
+        temp.append(row[input_index])
+
+    array = np.delete(array, input_index, axis=1)
+
+    temp = np.roll(temp, 1)
+    temp[-1] = temp[-2]
+    temp = np.asarray([temp])
+
+    con = np.concatenate((array, temp.T), axis=1)
+
+    return con
+
+if __name__ == '__main__':
+    array = [[1.0,2.0,3.0],\
+            [4.0, 5.0, 6.0]]
+
+    print(array_to_supervised(array, 0))
