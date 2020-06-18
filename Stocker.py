212891117af2af605575670338d4036f360f4bcc
CSLukeW <williams.luke.2001@gmail.com>
2020-06-17 19:35:54 -0600

    Support custom node counts and >1 stocks

--- Stocker.py
+++ Stocker.py
@@ -14,7 +14,7 @@ from sklearn.preprocessing import MinMaxScaler
 import helpers as helper
 
 class Stocker:
-    def __init__(self, symbol, data, depth, batch, test_size, loss, learning_rate, inpath):
+    def __init__(self, symbol, data, depth, node_counts, batch, test_size, loss, learning_rate, inpath):
         """ Creating Stocker instance immediately creates model 
 
             Model (WIP) is a two-layer LSTM. Defaults to Mean Squared Error
@@ -42,8 +42,8 @@ class Stocker:
         self.model = tf.keras.Sequential()
 
         for i in range(depth):
-            self.model.add(tf.keras.layers.LSTM(max(1, 100-i*30), activation='tanh', recurrent_activation='sigmoid', \
-                                                    input_shape=self.train_in.shape[-2:], return_sequences=True, name='Input'))
+            self.model.add(tf.keras.layers.LSTM(node_counts[i], activation='tanh', recurrent_activation='sigmoid', \
+                                                    input_shape=self.train_in.shape[-2:], return_sequences=True, name='LSTM'+str(i)))
             self.model.add(tf.keras.layers.Dropout(.3))
 
         self.model.add(tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(1)))
@@ -109,52 +109,55 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="Model Training Script")
     parser.add_argument('key', help='User API Key')
-    parser.add_argument('-depth', default=1, help='Depth of LSTM neural net, (default 1)')
-    parser.add_argument('-batch', default=50, help='Batch size (default 50)')
-    parser.add_argument('-test_size', default=.2, help='Percentage of samples to use for testing (decimal form)')
+    parser.add_argument('-depth', default=1, type=int, help='Depth of LSTM neural net, (default 1)')
+    parser.add_argument('-node_counts', default=[100], type=int, nargs='*', help='Node counts for each LSTM layer. (number of args must be equal to depth')
+    parser.add_argument('-batch', default=50, type=int, help='Batch size (default 50)')
+    parser.add_argument('-test_size', default=.2, type=float, help='Percentage of samples to use for testing (decimal form)')
     parser.add_argument('-loss', default='mse', help='Loss function for Neural Net')
-    parser.add_argument('-learning_rate', default=.001, help='Learning rate of Neural Net (Default .001)')
-    parser.add_argument('-epochs', default=100, help='Epoch count for training (Default 100)')
+    parser.add_argument('-learning_rate', default=.001, type=float, help='Learning rate of Neural Net (Default .001)')
+    parser.add_argument('-epochs', default=100, type=int, help='Epoch count for training (Default 100)')
     parser.add_argument('-model_in', default=None, help='Path of pre-made model to load')
     parser.add_argument('--early_stop', action='store_true', default=False, help='Apply early stopping to model training (Patience 10)')
     parser.add_argument('--plots', action='store_true', help='Saves all plots to plots folder')
-    parser.add_argument('symbol', help="List of symbols to train (Place all at end of command)")
+    parser.add_argument('symbols', nargs='*', help="List of symbols to train")
     parse = parser.parse_args()
 
     data = {}
-    symbol = parse.symbol
+    symbols = parse.symbols
 
-    # read historical daily data from alpha_vantage
-    # store in python dict
-    hist = helper.daily_adjusted(symbol, parse.key, compact=False)
-    hist = hist.drop(['6. volume', '7. dividend amount', '8. split coefficient'], axis=1)
-    hist = hist.reindex(index=hist.index[::-1])
-    data[symbol] = hist
-    print(hist)
-    print()
+    for symbol in symbols:
 
-    if parse.plots:
-        pyplot.figure()
-        hist.plot(subplots=True)
-        pyplot.suptitle('Input Features')
-        pyplot.savefig(helper.make_dir('./plots/' + symbol) + '/input.png')
+        # read historical daily data from alpha_vantage
+        # store in python dict
+        hist = helper.daily_adjusted(symbol, parse.key, compact=False)
+        hist = hist.drop(['6. volume', '7. dividend amount', '8. split coefficient'], axis=1)
+        hist = hist.reindex(index=hist.index[::-1])
+        data[symbol] = hist
+        print(hist)
+        print()
 
-    # test Stocker methods
-    model = Stocker(symbol, hist, parse.depth, parse.batch, parse.test_size, parse.loss, \
-                        parse.learning_rate, parse.model_in)
-    model.train(parse.epochs, parse.early_stop, parse.plots)
-    model.evaluate()
-    model.save_model()
-    predictions = model.predict_data(model.val_in)
+        if parse.plots:
+            pyplot.figure()
+            hist.plot(subplots=True)
+            pyplot.suptitle('Input Features')
+            pyplot.savefig(helper.make_dir('./plots/' + symbol) + '/input.png')
 
-    standard_numpy = hist[int(hist.shape[0]*(1-parse.test_size)):]['5. adjusted close'].to_numpy()
+        # test Stocker methods
+        model = Stocker(symbol, hist, parse.depth, parse.node_counts, parse.batch, parse.test_size, parse.loss, \
+                            parse.learning_rate, parse.model_in)
+        model.train(parse.epochs, parse.early_stop, parse.plots)
+        model.evaluate()
+        model.save_model()
+        predictions = model.predict_data(model.val_in)
 
-    if parse.plots:
-        pyplot.figure()
-        pyplot.plot(standard_numpy, label='True Values')
-        pyplot.plot(predictions[:, 0], label='Predictions')
-        pyplot.xlabel('Time Step')
-        pyplot.ylabel('Adjusted Close')
-        pyplot.suptitle('Predictions')
-        pyplot.legend()
-        pyplot.savefig(helper.make_dir('./plots/' + symbol) + '/predictions.png')
+        standard_numpy = hist[int(hist.shape[0]*(1-parse.test_size)):]['5. adjusted close'].to_numpy()
+
+        if parse.plots:
+            pyplot.figure()
+            pyplot.plot(standard_numpy, label='True Values')
+            pyplot.plot(predictions[:, 0], label='Predictions')
+            pyplot.xlabel('Time Step')
+            pyplot.ylabel('Adjusted Close')
+            pyplot.suptitle('Predictions')
+            pyplot.legend()
+            pyplot.savefig(helper.make_dir('./plots/' + symbol) + '/predictions.png')
