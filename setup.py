88688b4c87bcb5e1f1fd69aadabb4bf716ca0164
Luke WIlliams <williams.luke.2001@gmail.com>
2020-06-22 22:32:03 -0600

    Version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="StockerMake",
-    version="0.0.3",
+    version="0.0.31",
     author="Luke Williams",
     author_email="williams.luke.2001@gmail.com",
     description="Modular Neural Network Protyping for Stock Market Prediction",
